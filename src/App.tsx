9b168bcb613673dca2e78b1d0f70a144a5e930a3
Arthur Pacheco Alberge <arthurpa_@hotmail.com>
2023-08-25 22:30:09 -0300

    criar o projeto na web

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import NewPet from "./pages/NewPet/NewPet";
+import React from "react";
+import PetContext from "./contexts/PetContext";
+import "./App.css";
+import { CardHeader } from "@mui/material";
+
+const App = () => {
+  return (
+    <PetContext>
+      <Router>
+        <NavBar />
+        <CardHeader />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/new-pet" element={<NewPet />}></Route>
+          </Routes>
+      </Router>
+    </PetContext>
+  );
+};
+
+export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import PetCardTable from '../../components/PetCardTable/PetCardTable';
+import MultipleSelectCheckmarks from '../../components/Filter/Filter';
+import './Home.css'
+
+const Home = () => {
+  const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+  ];
+    return (
+        <>
+        <div className='sideBar'>
+          <MultipleSelectCheckmarks 
+          options={names}
+          filterName="Cidade"/>
+          <MultipleSelectCheckmarks 
+          options={names}
+          filterName="Abrigo"/>
+          <MultipleSelectCheckmarks 
+          options={names}
+          filterName="Idade"/>
+          <MultipleSelectCheckmarks 
+          options={names}
+          filterName="Tamanho"/>
+          <MultipleSelectCheckmarks 
+          options={names}
+          filterName="Cor"/>
+          <MultipleSelectCheckmarks 
+          options={names}
+          filterName="Genero"/>
+          <MultipleSelectCheckmarks 
+          options={names}
+          filterName="Habilidade"/>
+          <MultipleSelectCheckmarks 
+          options={names}
+          filterName="Amigavel com"/>
+          <MultipleSelectCheckmarks 
+          options={names}
+          filterName="Cuidados Especiais"/>
+        </div>
+        <div className='petsDisplay'>
+          <PetCardTable />
+        </div>
+        </>
+    );
+}
+
+export default Home;
